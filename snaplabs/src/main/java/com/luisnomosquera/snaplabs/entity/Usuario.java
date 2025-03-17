1bd7ed7b447dd4335eb2ce1c53bc075a7425d2af
Luis Alfonso No Mosquera <luisalfonsono@gmail.com>
2025-03-17 17:43:19 +0100

    feat: implementada entidad Usuario

--- snaplabs/src/main/java/com/luisnomosquera/snaplabs/entity/Usuario.java
+++ snaplabs/src/main/java/com/luisnomosquera/snaplabs/entity/Usuario.java
@@ -1,4 +1,21 @@
 package com.luisnomosquera.snaplabs.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
 public class Usuario {
+    @Id
+    private String uuid;
+    private String correo;
+    private String username;
+    private String password;
+    private String foto;
+    @Column(name = "fecha_creacion")
+    private LocalDate fechaCreacion;
 }
---
+++ snaplabs/src/main/java/com/luisnomosquera/snaplabs/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.luisnomosquera.snaplabs.repository;
+
+import com.luisnomosquera.snaplabs.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+}
