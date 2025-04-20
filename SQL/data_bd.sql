1041c47684ce971a456e6b04ee54fbe47afe7acf
Luis Alfonso No Mosquera <luisalfonsono@gmail.com>
2025-04-20 18:57:34 +0200

    feat: añadir archivos SQL

---
+++ SQL/data_bd.sql
@@ -0,0 +1,5 @@
+USE snaplabs;
+INSERT INTO `usuario` (uuid, correo, username, password, foto) 
+VALUES 
+    ('47c4e2d3-4f98-453b-ae97-218b68d446dd','luisalfonsono@gmail.com','ciro','$2a$10$SMLSb7G/O.EVCykTeD1XxOtATxsNvzmHnTV1nM/j888VZL4WKXQly','https://res.cloudinary.com/dhmmvfghx/image/upload/v1744658706/avatars/47c4e2d3-4f98-453b-ae97-218b68d446dd.png'),
+    ('63324757-d412-4d50-9ecc-137390f74946','luisalfonsono@gmail.com','luis','$2a$10$ZbykZ/0iFeuXXWQ7rcLo3OEOpMDfNK4tArsp3FwwdZDPPJxl3Nm5a','https://res.cloudinary.com/dhmmvfghx/image/upload/v1744621186/default_eb292v.png');
---
+++ SQL/estructura_bd.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE IF NOT EXISTS snaplabs;
+USE snaplabs;
+
+CREATE TABLE usuario (
+	uuid CHAR(36) PRIMARY KEY,
+	correo VARCHAR(255) NOT NULL,
+	username VARCHAR(25) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	foto VARCHAR(255) NOT NULL,
+	fecha_creacion date DEFAULT CURRENT_DATE,
+	UNIQUE (username)
+);
+
+CREATE TABLE mazo (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	contenido VARCHAR(255) NOT NULL,
+	nombre VARCHAR(25) NOT NULL,
+	descripcion TEXT DEFAULT '',
+	arquetipo VARCHAR(255) DEFAULT '',
+	publico BOOLEAN NOT NULL,
+	fecha_creacion date DEFAULT CURRENT_DATE,
+	id_usuario CHAR(36) NOT NULL,
+	CONSTRAINT fk_id_usuario_mazo FOREIGN KEY (id_usuario) REFERENCES usuario(uuid) ON DELETE CASCADE
+);
+
+CREATE TABLE variante (
+	uuid CHAR(36) PRIMARY KEY,
+	nombre VARCHAR(25) NOT NULL,
+	descripcion VARCHAR(255) DEFAULT '',
+	enlace VARCHAR(255) NOT NULL,
+	personaje VARCHAR(25) NOT NULL,
+	total_likes INT UNSIGNED DEFAULT 0,
+	fecha_creacion date DEFAULT CURRENT_DATE,
+	id_usuario CHAR(36) NOT NULL,
+	CONSTRAINT fk_id_usuario_variante FOREIGN KEY (id_usuario) REFERENCES usuario(uuid) ON DELETE CASCADE
+);
+
+CREATE TABLE usuario_favorito_mazo (
+	id_usuario CHAR(36),
+	id_mazo INT,
+	CONSTRAINT pk_usuario_favorito_mazo PRIMARY KEY (id_usuario,id_mazo),
+	CONSTRAINT fk_favorito_id_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(uuid) ON DELETE CASCADE,
+	CONSTRAINT fk_favorito_id_mazo FOREIGN KEY (id_mazo) REFERENCES mazo(id) ON DELETE CASCADE
+);
+
+CREATE TABLE carta (
+	nombre VARCHAR(50) PRIMARY KEY,
+	id_snap VARCHAR(50) NOT NULL
+);
