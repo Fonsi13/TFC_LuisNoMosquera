a8d035c3ff076696c85d3ecefdc2d4f0a521c975
Luis Alfonso No Mosquera <luisalfonsono@gmail.com>
2025-05-20 19:59:54 +0200

    feat: implementar filtros en vista Mazos

--- snaplabs/src/main/resources/static/js/mazo.js
+++ snaplabs/src/main/resources/static/js/mazo.js
@@ -1,9 +1,15 @@
 const imagenes = document.querySelectorAll(".gridCartas div");
+const fecha = document.getElementById("fecha");
+const arquetipos = document.querySelectorAll("input[name='arquetipo']");
+const gridMazos = document.querySelector(".gridMazos");
+const mazos = [...gridMazos.children];
 
 imagenes.forEach(imagen => {
-    imagen.addEventListener("mouseover", (event) => showTooltip(event));
+    imagen.addEventListener("mouseover", showTooltip);
     imagen.addEventListener("mouseleave", deleteTooltip);
 });
+fecha.addEventListener("change", ordenarMazosFecha);
+arquetipos.forEach(checkbox => checkbox.addEventListener("change", filtrarMazoByArquetipo));
 
 function showTooltip(event) {
     const url = event.currentTarget.getAttribute('data-src');
@@ -38,6 +44,32 @@ function showTooltip(event) {
 
 function deleteTooltip() {
     document.querySelectorAll(".tooltip").forEach(tooltip => tooltip.remove());
+}
+
+function ordenarMazosFecha(event) {
+    gridMazos.innerHTML = "";
+    asc = event.target.value === "asc";
+    mazos
+        .sort((a,b) => {
+        valorA = a.getAttribute("data-fecha");
+        valorB = b.getAttribute("data-fecha");
+        return asc ? new Date(valorA) - new Date(valorB) : new Date(valorB) - new Date(valorA);
+    })
+        .forEach(mazo => gridMazos.appendChild(mazo))
+}
+
+function filtrarMazoByArquetipo(event) {
+    gridMazos.innerHTML = "";
+    if (event.target.checked) {
+        arquetipos.forEach(checkbox => {
+            if (event.target !== checkbox && checkbox.checked) checkbox.checked = false;
+        });
+        mazos
+            .filter(mazo => mazo.getAttribute("data-arquetipo") === event.target.value)
+            .forEach(mazo => gridMazos.appendChild(mazo));
+    } else {
+        mazos.forEach(mazo => gridMazos.appendChild(mazo));
+    }
 }
 
 function copiarCodigo(event) {
